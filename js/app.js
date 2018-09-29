fa23f7f18d71a4e164ebe7066727ba45657d928c
sajicode <sajioloye@gmail.com>
2018-09-29 07:24:50 +0100

    add footer section

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ let buttons = document.querySelectorAll('.open')
 let close = document.getElementById('close');
 let modal = document.getElementById('modal');
 let images = document.querySelectorAll('.image');
-let modalImg = document.getElementById('modal-img')
+let modalImg = document.getElementById('modal-img');
+let time = document.getElementById("time");
 
-
+// enlarge image - open modal
 images.forEach(image => {
   if (image.classList.contains('image')) {
     image.addEventListener('click', function (e) {
@@ -16,7 +17,12 @@ images.forEach(image => {
   }
 });
 
+// close modal
 close.addEventListener('click', function (e) {
   e.preventDefault();
   modal.style.display = 'none';
 });
+
+// get current year
+let today = new Date().getFullYear();
+time.innerHTML = `Copyright &copy; ${today}`
